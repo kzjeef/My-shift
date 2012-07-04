26bb3ab9cc97adc1acd459c541ef60684ff73a9e
Zhang Jiejing <kzjeef@gmail.com>
2012-07-04 22:35:25 +0800

    add files missed.

---
+++ ShiftScheduler/config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* work day type config show as modal view or nagviation view. */
+#define CONFIG_WORKDAY_CONFIG_MODAL_VIEW 0
+
+/* Shift change functions. */
+#define CONFIG_SS_ENABLE_SHIFT_CHANGE_FUNCTION 0
+
+/* Use tab bar view controller in main screen.
+ * otherwise use a tabbar.*/
+#define CONFIG_MAIN_UI_USE_TAB_BAR_CONTROLLER 1
+
+#endif
+
