8c3b93a64bf9398a3d7cc86859fc6886e6d6f1e4
Zhang Jiejing <kzjeef@gmail.com>
2013-02-19 17:02:15 +0800

    SSKalDelegate:  pop a date picker view when user click the title view of Calendar.

--- ShiftScheduler/SSKalDelegate.m
+++ ShiftScheduler/SSKalDelegate.m
@@ -7,12 +7,64 @@
 //
 
 #import "SSKalDelegate.h"
+#import "SCModalPickerView.h"
+#import "NSDateAdditions.h"
+
+@interface SSKalDelegate()
+{
+    SCModalPickerView *_modalDatePickerView;
+    UIDatePicker *_datePicker;
+}
+
+@property (strong, readonly) UIDatePicker *datePicker;
+@property (strong, readonly) SCModalPickerView *modalDatePickerView;
+@end
 
 @implementation SSKalDelegate
 
 #pragma mark - KalViewControllerDelegate protocol.
 - (void) KalViewController:(KalViewController *)sender selectDate:(NSDate *)date
 {
+}
+
+- (UIDatePicker *)datePicker
+{
+    if (!_datePicker) {
+        _datePicker = [[UIDatePicker alloc] initWithFrame:CGRectMake(0, 250, 325, 250)];
+        CGSize pickerSize = [_datePicker sizeThatFits:CGSizeZero];
+        CGRect screenRect = [[UIScreen mainScreen] applicationFrame];
+        CGRect pickerRect = CGRectMake(0.0, screenRect.origin.y + screenRect.size.height - pickerSize.height - 65, pickerSize.width, pickerSize.height);
+        _datePicker.frame = pickerRect;
+        [_datePicker setDatePickerMode:UIDatePickerModeDate];
+    }
+
+    return _datePicker;
+}
+
+- (SCModalPickerView *)modalDatePickerView
+{
+    if (!_modalDatePickerView) {
+        _modalDatePickerView = [[SCModalPickerView alloc] init];
+    }
+
+    return _modalDatePickerView;
+}
+
+- (void) KalViewControllerdidSelectTitle:(KalViewController *)sender
+{
+
+    [self.modalDatePickerView setPickerView:self.datePicker];
+    [self.modalDatePickerView setPickerView:self.datePicker];
+    __weak UIDatePicker *pdatePicker = self.datePicker;
+    [self.modalDatePickerView setCompletionHandler:^(SCModalPickerViewResult result) {
+            if (result == SCModalPickerViewResultDone) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                        [sender showAndSelectDate:[pdatePicker.date cc_dateByMovingToMiddleOfDay]];
+                    });
+            }
+        }];
+
+    [self.modalDatePickerView show];
 }
 
 #pragma mark UITableViewDelegate protocol conformance
