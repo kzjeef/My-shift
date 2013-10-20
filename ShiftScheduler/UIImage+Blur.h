e4cdf7bd976693977166aa29738d9289560b6753
Zhang Jiejing <kzjeef@gmail.com>
2013-10-20 18:34:57 +0800

    add reveal framwork

---
+++ ShiftScheduler/UIImage+Blur.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+Blur.h
+//  ShiftScheduler
+//
+//  Created by Jiejing Zhang on 13-10-20.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Blur)
+
+@end
---
+++ ShiftScheduler/UIImage+Blur.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+Blur.m
+//  ShiftScheduler
+//
+//  Created by Jiejing Zhang on 13-10-20.
+//
+//
+
+#import "UIImage+Blur.h"
+
+@implementation UIImage (Blur)
+
+@end
